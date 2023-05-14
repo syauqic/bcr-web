d725dc8738a1850820420edfb8620b3b111c0a37
syauqic <syauqi.benaputra1201@gmail.com>
2023-05-14 22:15:25 +0700

    debug hero

--- src/pages/Home/components/Hero.jsx
+++ src/pages/Home/components/Hero.jsx
@@ -81,6 +81,35 @@ export const Hero = () => {
           </div>
         </div>
       </div>
+
+      <div
+        className="offcanvas offcanvas-end"
+        tabIndex={-1}
+        id="offcanvasExample"
+        aria-labelledby="offcanvasExampleLabel"
+      >
+        <div className="offcanvas-header">
+          <h5 className="offcanvas-title fw-bold" id="offcanvasExampleLabel">
+            BCR
+          </h5>
+          <button
+            type="button"
+            className="btn-close"
+            data-bs-dismiss="offcanvas"
+            aria-label="Close"
+          />
+        </div>
+        <div className="offcanvas-body">
+          <div className="menu">
+            <ul>
+              <li>Our Services</li>
+              <li>Why Us</li>
+              <li>Testimonial</li>
+              <li>FAQ</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
